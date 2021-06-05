a5e4a024c965be90f7d7fa991290f56446f06661
Matheus P. Agostinho <mat.matheus_pa@hotmail.com>
2021-06-05 13:13:47 -0300

    rename

--- src/main/java/com/greens/discovery/Discovery.java
+++ src/main/java/com/greens/discovery/Discovery.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.greens.discovery;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
@@ -19,13 +19,12 @@ import org.apache.logging.log4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("examplemod")
-public class ExampleMod
-{
+@Mod("discovery")
+public class Discovery {
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
 
-    public ExampleMod() {
+    public Discovery() {
         // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         // Register the enqueueIMC method for modloading
@@ -39,8 +38,7 @@ public class ExampleMod
         MinecraftForge.EVENT_BUS.register(this);
     }
 
-    private void setup(final FMLCommonSetupEvent event)
-    {
+    private void setup(final FMLCommonSetupEvent event){
         // some preinit code
         LOGGER.info("HELLO FROM PREINIT");
         LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
